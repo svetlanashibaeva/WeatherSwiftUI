c25378448e5fce494675a1e5a89b05787e67e974
Shibaeva Svetlana <svetlanashibaeva96@gmail.com>
2023-11-16 12:35:51 +0500

    Initial Commit

---
+++ WeatherSwiftUI/Views/Main/HomeView.swift
@@ -0,0 +1,113 @@
+//
+//  HomeView.swift
+//  WeatherSwiftUI
+//
+//  Created by Светлана Шибаева on 13.11.2023.
+//
+
+import SwiftUI
+import BottomSheet
+
+enum BottomSheetPosition: CGFloat, CaseIterable {
+    case top = 0.83 // 702/844
+    case middle = 0.385 // 325/844
+}
+
+struct HomeView: View {
+    @State var bottomSheetPosition: BottomSheetPosition = .middle
+    @State var bottomSheetTranslation: CGFloat = BottomSheetPosition.middle.rawValue
+    @State var hasDragged: Bool = false
+    
+    var bottomSheetTranslationProrated: CGFloat {
+        (bottomSheetTranslation - BottomSheetPosition.middle.rawValue) / (BottomSheetPosition.top.rawValue - BottomSheetPosition.middle.rawValue)
+    }
+    
+    var body: some View {
+        NavigationView {
+            GeometryReader { geometry in
+                let screenHeight = geometry.size.height + geometry.safeAreaInsets.top + geometry.safeAreaInsets.bottom
+                let imageOffset = screenHeight + 36
+                ZStack {
+                    Color.background
+                        .ignoresSafeArea()
+                    
+                    Image("Background")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .offset(y: -bottomSheetTranslationProrated * imageOffset)
+                    
+                    Image("House")
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        .padding(.top, 257)
+                        .offset(y: -bottomSheetTranslationProrated * imageOffset)
+                    
+                    VStack(spacing: -10 * (1 - bottomSheetTranslationProrated)) {
+                        Text("Moscow")
+                            .font(.largeTitle)
+                        
+                        VStack {
+                            Text(attributedString)
+                            
+                            Text("H:24°   L:18°")
+                                .font(.title3.weight(.semibold))
+                                .opacity(1 - bottomSheetTranslationProrated)
+                        }
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 51)
+                    .offset(y: -bottomSheetTranslationProrated * 46)
+                    
+                    // MARK: Bottom sheet
+                    BottomSheetView(position: $bottomSheetPosition) {
+    //                    Text(bottomSheetPosition.rawValue.formatted())
+                    } content: {
+                        ForecastView(bottomSheetTranslationProrated: bottomSheetTranslationProrated)
+                    }
+                    .onBottomSheetDrag { translation in
+                        bottomSheetTranslation = translation / screenHeight
+                        
+                        withAnimation(.easeInOut) {
+                            hasDragged = bottomSheetPosition == BottomSheetPosition.top ? true : false
+                        }
+                    }
+                    
+                    // MARK: Tab bar
+                    TabBar(action: {
+                        bottomSheetPosition = .top
+                    })
+                    .offset(y: bottomSheetTranslationProrated * 115)
+                }
+            }
+            .navigationBarBackButtonHidden(true)
+        }
+    }
+    
+    var attributedString: AttributedString {
+        var string = AttributedString("19°" + (hasDragged ? " | " : "\n ") + "Mostly clear")
+        
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: (96 - (bottomSheetTranslationProrated * (96 - 20))), weight: hasDragged ? .semibold : .thin)
+            string[temp].foregroundColor = hasDragged ? .secondary : .primary
+        }
+        
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        
+        if let weather = string.range(of: "Mostly clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        
+        return string
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Светлана Шибаева on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
